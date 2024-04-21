4c250de25eecf1021587470757660ea47d952e79
zhamdana <zaharadeenhamdana@gmail.com>
2024-04-21 13:50:32 +0000

    Bulk

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-#Install the puppet-lint package
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Installs puppet-lint, version 2.1.1
+
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
