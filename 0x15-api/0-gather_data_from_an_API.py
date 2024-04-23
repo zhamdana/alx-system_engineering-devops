c72969ea0da2d635136dd3a302f0a49ad29c7d6d
zhamdana <zaharadeenhamdana@gmail.com>
2024-04-23 12:39:39 +0000

    Bulk

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,57 +1,14 @@
 #!/usr/bin/python3
-"""This module is in charge of making the connection with the api and
-    prints information about a employee TODO list progress."""
-from sys import argv
+"""Returns to-do list information for a given employee ID."""
 import requests
+import sys
 
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-def connection():
-    """This method is responsible for making the connection with the api
-        and decoding the json format.
-
-    Returns:
-        dict: Users and tasks in dictionary format.
-    """
-    tasks = requests.get("https://jsonplaceholder.typicode.com/todos")
-    users = requests.get("https://jsonplaceholder.typicode.com/users")
-    return tasks.json(), users.json()
-
-
-def main_function(employee_id):
-    """This method is in charge of processing the task and user
-        dictionaries to print useful information.
-
-    Args:
-        employee_id (int): User identifier number.
-    """
-    if not employee_id.isdigit():
-        return
-
-    employee_id = int(employee_id)
-    tasks_content, users_content = connection()
-
-    for user in users_content:
-        if user.get("id") == employee_id:
-            break
-    else:
-        return
-
-    total = 0
-    completed = []
-    for task in tasks_content:
-        if task.get("userId") == employee_id:
-            total += 1
-            if task.get("completed") is True:
-                completed.append(task.get("title"))
-    if total == 0:
-        return
-
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), total))
-    for ctask in completed:
-        print("\t {}".format(ctask))
-
-
-if __name__ == '__main__':
-    if len(argv) == 2:
-        main_function(argv[1])
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
